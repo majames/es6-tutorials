beab78c1431600037e7dc34bde724c71100568bf
majames <majames91@gmail.com>
2015-09-12 10:40:43 +1000

    Updated tutorial comment

--- esnext-generation/tut04.js
+++ esnext-generation/tut04.js
@@ -1,3 +1,6 @@
+// generators are used to generate itterators without having to write the
+// boiler plate code for the next() method. Instead the 'yield' keyword is used
+// execution halts when a yield statement is reached
 
 var generate = function*(isEven) {
   var value = isEven ? 0 : -1;
